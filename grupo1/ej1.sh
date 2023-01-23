48d651e84975da1feedd63760518ea5e951b132a
kikso <quiquevolc@gmail.com>
2023-01-23 09:58:06 +0100

    ej1 resuelto

--- grupo1/ej1.sh
+++ grupo1/ej1.sh
@@ -1,2 +1,10 @@
 #! /bin/bash
 
+echo "Cual es tu nombre: ";
+read nombre;
+echo "Tu primer apellido: ";
+read apellido1;
+echo "Tu segundo apellido ";
+read apellido2;
+sleep 5;
+echo "Este es tu nombre completo: $nombre $apellido1 $apellido2";
